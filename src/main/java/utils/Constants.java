5caf8f37b22767a72543bb1324251d7f5151fb6f
Sergey_Potapov <s.potapov@andersenlab.com>
2017-11-29 17:59:26 +0200

    was added implementation reporting, logging, multithreading and also was added readme file

--- src/main/java/utils/Constants.java
+++ src/main/java/utils/Constants.java
@@ -1,11 +1,26 @@
 package utils;
 
+/**
+ * @author Sergey Potapov
+ */
 public class Constants {
-
     //========================BASE URL==================================
     public static final String BASE_URL = "http://tomsk.demo.dev.magonline.ru";
 
-    //========================BROWSER DRIVER NAMES==================================
+    //========================BROWSER DRIVER NAMES======================
     public static final String DRIVER_NAME_FIREFOX = "webdriver.gecko.driver";
     public static final String DRIVER_NAME_CHROME = "webdriver.chrome.driver";
- }
+
+    //========================BROWSER DRIVER PATH=======================
+    public static final String FIREFOX_DRIVER_PATH = System.getProperty("user.dir") + "/src/main/resources/drivers/geckodriver.exe";
+    public static final String CHROME_DRIVER_PATH = System.getProperty("user.dir") + "/src/main/resources/drivers/chromedriver.exe";
+
+    //========================SCREENSHOT PATH===========================
+    public static final String ERROR_SCREENSHOT_FOLDER = System.getProperty("user.dir") + "/test-output/screenShots/ErrorScreenShot/";
+    public static final String SUCCESS_SCREENSHOT_FOLDER = System.getProperty("user.dir") + "/test-output/screenShots/SuccessScreenShot/";
+
+
+    //LOGGER FILES PATH
+    public static final String LOG_FOLDER = System.getProperty("user.dir") + "/test-output/log/testlog.log";
+
+}
--- src/test/java/regressionSuit/base/BaseTest.java
+++ src/test/java/regressionSuit/base/BaseTest.java
@@ -1,9 +1,21 @@
 package regressionSuit.base;
 
 import driverFactory.DriverFactory;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import static utils.Constants.ERROR_SCREENSHOT_FOLDER;
+import static utils.Constants.LOG_FOLDER;
+import static utils.Constants.SUCCESS_SCREENSHOT_FOLDER;
 
 /**
  * @author Sergey Potapov
@@ -13,16 +25,74 @@ public class BaseTest {
     protected WebDriver driver;
 
     /**
-     * Set browser before starting auto tests
+     * Clean directory with error and success screenshots before starting auto tests
+     * and set browser before starting auto tests
      */
     @BeforeTest
     public void setUp() {
-        driver = DriverFactory.setDriver("Firefox");
+        driver = DriverFactory.setDriver("Chrome");
+        if (new File(ERROR_SCREENSHOT_FOLDER).exists())
+            try {
+                FileUtils.cleanDirectory(new File(ERROR_SCREENSHOT_FOLDER));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+
+        if (new File(ERROR_SCREENSHOT_FOLDER).exists())
+            try {
+                FileUtils.cleanDirectory(new File(SUCCESS_SCREENSHOT_FOLDER));
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        /**
+         * Clean log directory before starting
+         */
+        if (new File(LOG_FOLDER).exists()) {
+
+            File[] files = new File(LOG_FOLDER).listFiles();
+
+            if (files != null) {
+                for (File file : files) {
+                    try {
+                        if (file.exists()) {
+                            FileWriter fwOb = new FileWriter(file, false);
+                            PrintWriter pwOb = new PrintWriter(fwOb, false);
+                            pwOb.flush();
+                            pwOb.close();
+                            fwOb.close();
+                        }
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Method for screenshot creation
+     *
+     * @param screenShotName-name of screenshot
+     * @param folder-             folder which contain screenshots
+     * @return dest - destination where to be situated screenshots
+     */
+    public static String capture(String screenShotName, String folder) {
+        TakesScreenshot takesScreenshot = ((TakesScreenshot) DriverFactory.getDriver());
+        File source = takesScreenshot.getScreenshotAs(OutputType.FILE);
+        String dest = folder + screenShotName + ".png";
+        File destination = new File(dest);
+        try {
+            FileUtils.copyFile(source, destination);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dest;
     }
 
     /**
      * Method for closing browser and auto tests
      */
+
     @AfterTest
     public void tearDown() {
         driver.close();
