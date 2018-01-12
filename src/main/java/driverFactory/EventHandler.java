936a22044c3f4397409504e3c2b0e47900279854
Reinhardt <ru.sheyko@andersenlab.com>
2018-01-12 10:47:26 +0200

    refactoring 'get Test Name', remove unusable method

--- src/main/java/driverFactory/EventHandler.java
+++ src/main/java/driverFactory/EventHandler.java
@@ -1,7 +1,5 @@
 package driverFactory;
 
-import basePage.BasePage;
-import basePage.BasePage.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
