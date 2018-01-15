c91ac30517c269ad4439e73c0cccf787ba429cb9
Sergey_Potapov <s.potapov@andersenlab.com>
2018-01-15 17:28:17 +0200

    [C37423-C37424] [C37479][C37564-C37565] [C37567] [C37480][ C37485-C37486] - creation autotests

--- src/main/java/pages/RegistrationPage.java
+++ src/main/java/pages/RegistrationPage.java
@@ -652,4 +652,75 @@ public class RegistrationPage extends BasePage {
         AssertCollector.assertEquals(legalAddress.getAttribute("value").length(), " Number of symbols is equal ",
                 RandomStringUtils.randomAlphanumeric(255).length());
     }
+
+    public void verifyCompanyFieldPresence() {
+        getUrl(REGISTRATION_PAGE_URL);
+        elementFluentWaitVisibility(organizationButton, driver).click();
+        AssertCollector.assertTrue(company.isDisplayed());
+    }
+
+    //not pass
+    public void verifyMaximumInputInCompany() {
+        getUrl(REGISTRATION_PAGE_URL);
+        elementFluentWaitVisibility(organizationButton, driver).click();
+        fillInputField(company, driver, RandomStringUtils.randomAlphanumeric(256));
+        AssertCollector.assertEquals(company.getAttribute("value").length(), " Number of symbols is equal ",
+                RandomStringUtils.randomAlphanumeric(255).length());
+    }
+
+    public void verifyAddressFieldPresence() {
+        getUrl(REGISTRATION_PAGE_URL);
+        elementFluentWaitVisibility(organizationButton, driver).click();
+        AssertCollector.assertTrue(address.isDisplayed());
+    }
+
+    public void verifyPromptsInAddressFieldAfterInput() {
+        getUrl(REGISTRATION_PAGE_URL);
+        elementFluentWaitVisibility(organizationButton, driver).click();
+        fillInputField(address, driver, "Т");
+        textPresent("Выберите адрес");
+    }
+
+    public void verifyPromptChoosingInAddressFieldAfterInput() {
+        getUrl(REGISTRATION_PAGE_URL);
+        elementFluentWaitVisibility(organizationButton, driver).click();
+        scrollToNecessaryElement(company);
+        elementFluentWaitVisibility(address, driver).click();
+        fillInputField(address, driver, "Томск");
+        elementFluentWaitVisibility(cityForAddress, driver).click();
+        AssertCollector.assertTrue(address.getAttribute("value").contains("Томск"));
+        textPresent("Внимание! Вы не указали номер квартиры, офиса.");
+    }
+
+    //not pass
+    public void verifyFullAddressInputInAddressField() {
+        getUrl(REGISTRATION_PAGE_URL);
+        elementFluentWaitVisibility(organizationButton, driver).click();
+        scrollToNecessaryElement(company);
+        fillInputField(address, driver, "Томск, пр. Мира 20, кв. 7");
+    }
+
+    //not pass
+    public void verifyMaximumInputInAddressField() {
+        getUrl(REGISTRATION_PAGE_URL);
+        elementFluentWaitVisibility(organizationButton, driver).click();
+        fillInputField(address, driver, RandomStringUtils.randomAlphanumeric(256));
+        AssertCollector.assertEquals(address.getAttribute("value").length(), " Number of symbols is equal ",
+                RandomStringUtils.randomAlphanumeric(255).length());
+    }
+
+    public void verifyCommentsFieldPresence() {
+        getUrl(REGISTRATION_PAGE_URL);
+        elementFluentWaitVisibility(organizationButton, driver).click();
+        AssertCollector.assertTrue(comments.isDisplayed());
+    }
+
+    //not pass
+    public void verifyMaximumInputInCommentsField() {
+        getUrl(REGISTRATION_PAGE_URL);
+        elementFluentWaitVisibility(organizationButton, driver).click();
+        fillInputField(comments, driver, RandomStringUtils.randomAlphanumeric(1001));
+        AssertCollector.assertEquals(comments.getAttribute("value").length(), " Number of symbols is equal ",
+                RandomStringUtils.randomAlphanumeric(1000).length());
+    }
 }
