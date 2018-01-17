84c1dae15fbcf4f7781726d8f338798a59c178ef
Reinhardt <ru.sheyko@andersenlab.com>
2018-01-17 09:32:02 +0200

    refactorin category page , all test is active (39075,39078,39081,39083,390206,40223)

--- src/test/java/testcases/categoryPage/categoryTest.java
+++ src/test/java/testcases/categoryPage/categoryTest.java
@@ -17,76 +17,30 @@ public class categoryTest extends BaseTest {
 
 	@Test
 	public void verifyHeader() {
-		TestReporter.testTitle("Test ID - C39080");
-		categoryPage.header();
+		TestReporter.testTitle("Test ID - C39075");
+		categoryPage.selectFromCategoryDropDown();
 		
 	}
 	@Test
 	public void verifyCommodityGrid() {
 		TestReporter.testTitle("Test ID - C39081");
-		categoryPage.commodityGridList("Grid");
+		categoryPage.commodityGridList();
 
-	}
-	@Test
-	public void verifyCommodityList() {
-		TestReporter.testTitle("Test ID - C39082");
-		categoryPage.commodityGridList("List");
-
-	}
-	@Test
-	public void verifyStatusBlock() {
-		TestReporter.testTitle("Test ID - C39083");
-		categoryPage.statusBlock();
 	}
 	@Test
 	public void verifyCheckBoxIsPresent() {
-		TestReporter.testTitle("Test ID - C39086");
-		categoryPage.CheckBox("Is Present");
+		TestReporter.testTitle("Test ID - C39083");
+		categoryPage.CheckBox();
 	}
 	@Test
-	public void verifyBlockSelectedParameter() {
-		TestReporter.testTitle("Test ID - C39088");
-		categoryPage.CheckBox("Is Active");
-	}
-	@Test
-	public void verifyDeletedBtnFromBlockSelectedParameter() {
-		TestReporter.testTitle("Test ID - C39089");
-		categoryPage.CheckBox("DeleteBtn All");
-	}
-	@Test
-	public void verifyDeletedAllFromBlockSelectedParameter() {
-		TestReporter.testTitle("Test ID - C39090");
-		categoryPage.CheckBox("Delete All");
-	}
-	@Test
-	public void verifySortDefaultAsc() {
+	public void verifySortDefault() {
 		TestReporter.testTitle("Test ID - C39206");
-		categoryPage.sortFilter("Asc");
-	}
-	@Test
-	public void verifySortDefaultDesc() {
-		TestReporter.testTitle("Test ID - C39215");
-		categoryPage.sortFilter("Deasc");
+		categoryPage.sortFilterDefault();
 	}
 	@Test
 	public void verifySortPriceDesc() {
-		TestReporter.testTitle("Test ID - C39218");
-		categoryPage.sortFilter("Price Deasc");
+		TestReporter.testTitle("Test ID - C40223");
+		categoryPage.sortFilterPrice();
 	}
-	@Test
-	public void verifySortPriceAsc() {
-		TestReporter.testTitle("Test ID - C39220");
-		categoryPage.sortFilter("Price Asc");
-	}
-	@Test
-	public void verifyCategoryFromSearch() {
-		TestReporter.testTitle("Test ID - C39220");
-		categoryPage.selectFromSearch();
-	}
-	@Test
-	public void verifyCategoryFromDropMenu() {
-		TestReporter.testTitle("Test ID - C39220");
-		categoryPage.selectFromDropDown();
-	}
-	
+
 }
